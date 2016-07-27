42cf6558eeefc601b9db0c6cee3a21a3e9fcf1ca
nemesis <bondarenko.kirill@gmail.com>
2016-07-27 15:01:14 +0300

    change version

--- src/FilterAndSorting.php
+++ src/FilterAndSorting.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
  * Class FilterAndSorting
  * @package App\Traits
  *
- * @version 1.0.3
+ * @version 1.0.4
  * @author Bondarenko Kirill <bondarenko.kirill@gmail.com>
  */
 trait FilterAndSorting
